4dce3afeb4f16776dd7a1f7bea080be639ec5d20
kartikmukati <kartikmukati2021@gmail.com>
2024-06-24 01:13:27 +0530

    progress bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,37 @@
-function App() {
+import './App.css'
+
+import styled, {keyframes} from 'styled-components';
+
+function App(props) {
+
+  const progress_ = props.percentage
+
   return (
-    <>
-    Hello ! React Machine Code YT Series
-    </>
+    <div className="container">
+      <Progress y={props.percentage}>
+        <h1>{progress_}%</h1>
+      </Progress>
+    </div>
   )
 }
 
 export default App
+
+const progressAnimation = (percentage) => keyframes`
+  0% {width: 0%;}
+  100% { width: ${percentage}%;}
+`
+
+
+const Progress = styled.div`
+  height: 100%;
+  width: auto;
+  border-radius: 16px;
+  background-color: red;
+  display: flex;
+  align-items: center;
+  justify-content: end;
+  animation: ${(props) => progressAnimation(props.y)} 2s forwards
+`
+
+
