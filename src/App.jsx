1a66a7aec38615be9aaac6867508842a234cf95d
kartikmukati <kartikmukati2021@gmail.com>
2024-07-07 17:41:35 +0530

    progress step bar in reactjs

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,63 @@
+import { useState } from 'react'
 import './App.css'
 
 import { styled, keyframes } from 'styled-components'
 
 function App(props) {
 
-  const progress = props.percentage;
+  const arr = [1,2,3,4,5,6]
+
+  const percentageEachStep = 100 / (arr.length - 1);
+
+  const [currentStep, setCurrentStep] = useState(arr[0]);
+  const [percentageStart, setPercentageStart] = useState(0);
+  const [percentageCompleted, setPercentageCompleted] = useState(0);
+
+  const nextStepClickHandler = () => {
+    setCurrentStep((pre) => {return pre + 1});
+    setPercentageStart(percentageCompleted);
+    setPercentageCompleted((pre) => {return pre + percentageEachStep});
+  }
 
   return (
-    <OuterCircle percentage={progress}>
-    <div className='inner-circle'>
-      <h1>{progress}%</h1>
+    <>
+    <div className='container'>
+      <div className='progress-line-outer'>
+        <ProgressLineInner $percentageStart={percentageStart} $percentageCompleted={percentageCompleted}>
+          <div className='steps-ctn'>
+            {arr.map((item, index) => {
+              return (
+                <div className={currentStep === item ? 'progress-current-circle' : (currentStep > item ? 'progress-circle-filled' : 'progress-circle')}>
+                  <h1>{item}</h1>
+                </div>
+              )
+            })}
+          </div>
+        </ProgressLineInner>
+      </div>
+
     </div>
-    </OuterCircle>
+    <button onClick={nextStepClickHandler}>Next Step</button>
+    </>
   )
 }
 
 export default App
 
-const progressAnimate = (percentage) => keyframes`
-  0% {background: conic-gradient(blue 0deg, rgb(228, 225, 225) 0deg);}
-  100% {background: conic-gradient(blue ${percentage / 100 * 360}deg, rgb(228, 225, 225) 0deg);}
+const progress = (percentageStart, percentageCompleted) => keyframes`
+  from {
+    width: ${percentageStart}%;
+  }
+  to {
+    width: ${percentageCompleted}%;
+  }
 `
 
-const OuterCircle = styled.div`
-     width: 500px;
-     height: 500px;
-     border-radius: 50%;
+
+const ProgressLineInner = styled.div`
+     height: 2px;
+     background-color: rgb(0, 174, 255);
      display: flex;
-     justify-content: center;
      align-items: center;
-     animation: ${(props) => progressAnimate(props.percentage)} 2s forwards;
+     animation: ${(props) => progress(props.$percentageStart, props.$percentageCompleted)} 2s forwards
 `
