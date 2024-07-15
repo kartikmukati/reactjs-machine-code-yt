447c22df03b9cca64a12ca35ad76c47bac851689
kartikmukati <kartikmukati2021@gmail.com>
2024-07-16 00:16:37 +0530

    image carousel click to swipe

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,59 @@
-import { useState } from 'react';
-import './App.css'
+import { useRef } from "react";
+import "./App.css";
 
-function App(props) {
 
-  const [currentStar, setCurrentStar] = useState(-1);
-  const [starHover, setStarHover] = useState(-1);
+function App() {
 
-  const starClickedHandler = (index) => {
-    setCurrentStar(index + 1);
+  const ref = useRef();
+  
+  const imageArrSocial = [
+    "/images/x.png",
+    "/images/twitter.png",
+    "/images/amazon.png",
+    "/images/netflix.png",
+    "/images/apple.png",
+    "/images/facebook.png",
+    "/images/instagram.png",
+    "/images/x.png",
+    "/images/twitter.png",
+    "/images/amazon.png",
+    "/images/netflix.png",
+    "/images/apple.png",
+    "/images/facebook.png",
+    "/images/instagram.png",
+  ];
+
+  const prevClickHandler = () => {
+    const scrollLeft = ref.current.scrollLeft;
+    const itemWidth = parseInt(getComputedStyle(ref.current.children[0]).width)
+    ref.current.scrollLeft = scrollLeft - itemWidth;
   }
 
-  const mouseHoverHandler = (index) => {
-    setStarHover(index + 1);
+  const nextClickHandler = () => {
+    const scrollLeft = ref.current.scrollLeft;
+    const itemWidth = parseInt(getComputedStyle(ref.current.children[0]).width)
+    ref.current.scrollLeft = scrollLeft + itemWidth;
   }
 
   return (
-   <div className='container'>
-    {[...Array(5)].map((item, index) => {
-      return (
-        <div className={(starHover || currentStar) > index ? 'star-filled' : 'star'} key={index} 
-        onClick={() => starClickedHandler(index)}
-        onMouseEnter={() => mouseHoverHandler(index)}
-        onMouseLeave={() => setStarHover(null)}></div>
-      )
-    })}
-   </div>
-  )
+    <>
+      <div className="containers shadow-sm p-3 mb-5 bg-body-tertiary rounded" >
+        <div className="item-list" ref={ref}>
+          {imageArrSocial.map((item, index) => {
+            return (
+              <div>
+                <img className="item" src={item} />
+              </div>
+            );
+          })}
+        </div>
+      </div>
+      <div className="btn-ctn">
+        <button className="btn btn-outline-primary" onClick={prevClickHandler}>Prev</button>
+        <button className="btn btn-outline-primary" onClick={nextClickHandler}>Next</button>
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
