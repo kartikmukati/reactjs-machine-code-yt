cba86f6d08cbea51736d0a901772c22544fb7606
kartikmukati <kartikmukati2021@gmail.com>
2024-07-23 16:50:06 +0530

    ipad scroll animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,58 +1,30 @@
-import { useRef } from "react";
 import "./App.css";
-
 
 function App() {
 
-  const ref = useRef();
-  
-  const imageArrSocial = [
-    "/images/x.png",
-    "/images/twitter.png",
-    "/images/amazon.png",
-    "/images/netflix.png",
-    "/images/apple.png",
-    "/images/facebook.png",
-    "/images/instagram.png",
-    "/images/x.png",
-    "/images/twitter.png",
-    "/images/amazon.png",
-    "/images/netflix.png",
-    "/images/apple.png",
-    "/images/facebook.png",
-    "/images/instagram.png",
-  ];
-
-  const prevClickHandler = () => {
-    const scrollLeft = ref.current.scrollLeft;
-    const itemWidth = parseInt(getComputedStyle(ref.current.children[0]).width)
-    ref.current.scrollLeft = scrollLeft - itemWidth;
-  }
-
-  const nextClickHandler = () => {
-    const scrollLeft = ref.current.scrollLeft;
-    const itemWidth = parseInt(getComputedStyle(ref.current.children[0]).width)
-    ref.current.scrollLeft = scrollLeft + itemWidth;
-  }
-
   return (
-    <>
-      <div className="containers shadow-sm p-3 mb-5 bg-body-tertiary rounded" >
-        <div className="item-list" ref={ref}>
-          {imageArrSocial.map((item, index) => {
-            return (
-              <div>
-                <img className="item" src={item} />
-              </div>
-            );
-          })}
-        </div>
+    <div className="scroller">
+      {/* SVG PAth */}
+      <div className="mask">
+        <svg viewBox="0 0 1025 437" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <g clipPath="url(#clip0_3_17)">
+                  <rect width="1024.6" height="437" fill="white" />
+                  <path
+                    d="M1056 390.5L500 483L1029.5 312L226.5 467.5L1010 238.5L-14.5 442.5L956.5 174.5L26.5 349.5L854.5 104L107.5 251.5L755 34.5L277.5 118L396.5 50"
+                    stroke="black" strokeWidth="5" strokeLinecap="round" strokeLinejoin="round" pathLength="1" />
+                </g>
+                <defs>
+                  <clipPath id="clip0_3_17">
+                    <rect width="1024.6" height="837" fill="none" />
+                  </clipPath>
+                </defs>
+              </svg>
       </div>
-      <div className="btn-ctn">
-        <button className="btn btn-outline-primary" onClick={prevClickHandler}>Prev</button>
-        <button className="btn btn-outline-primary" onClick={nextClickHandler}>Next</button>
+      {/* Video */}
+      <div className="video">
+      <video autoPlay playsInline muted disablePictureInPicture disableRemotePlayback loop src="https://www.apple.com/105/media/ww/ipad-10.9/2022/4c5d6d90-d0de-429a-84f7-cf8827181a11/anim/features/large_2x.mp4"></video>
       </div>
-    </>
+    </div>
   );
 }
 
